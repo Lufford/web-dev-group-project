41bfe351c00efe9610cb77b80e12e031e3b6877c
BreezyPotato <kristopher.prince.27@gmail.com>
2025-11-14 16:10:41 -0500

    Update UsersAuth.js
    UserInfo Reference

--- project-backend/Models/UsersAuth.js
+++ project-backend/Models/UsersAuth.js
@@ -4,7 +4,8 @@ const userAuthSchema = new mongoose.Schema(
     {
         name: {type: String, required: true, trim:true},
         email: {type: String, required: true, unique: true, lowercase: true, trim: true},
-        passwordHash: {type: String, required: true}
+        passwordHash: {type: String, required: true},
+        userInfo: {type: mongoose.Schema.Types.ObjectId, ref: 'UserInfo', required: true }
     },
     { timestamps: true}
 
