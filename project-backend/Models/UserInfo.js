1a3f7ce9d8b98b7441812936fe65c55f865b0c33
NatashyaPeddle <natashya.peddle@gmail.com>
2025-11-14 16:00:01 -0500

    UserInfo Model Schema Added

---
+++ project-backend/Models/UserInfo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userInfoSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true, trim: true},
+        address: {type: String, required: true, trim: true}
+    }
+);
+
+const UserInfo = mongoose.model('UserInfo', userInfoSchema);
+
+module.exports = UserInfo;
