77a0bc40c2247e65f1fcba38bcf1c2907ef2e3c3
Drayton D'Cruz <ddcruz2@my.centennialcollege.ca>
2025-11-17 12:38:00 -0500

    itemRouteUpdate

--- project-backend/Models/Items.js
+++ project-backend/Models/Items.js
@@ -4,7 +4,9 @@ const itemSchema = new mongoose.Schema(
     {
         name: {type: String, required: true, trim: true},
         price: {type: Number, required: true, trim: true},
-        userInfo: {type: mongoose.Schema.Types.ObjectId, ref: "UserInfo"}
+        userInfo: {type: mongoose.Schema.Types.ObjectId, ref: "UserInfo"},
+        vendor: {type: mongoose.Schema.Types.ObjectId, ref: "UserAuth", required: true }
+
     },
     { timestamps: true}
 );
--- project-backend/Routes/itemRoute.js
+++ project-backend/Routes/itemRoute.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const Item = require("../Models/Items.js"); 
 
 const requireAuth = (req, res, next) => {
     try {
