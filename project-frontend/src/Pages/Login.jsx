0ffa0fd2068084cc21f7ae9c2478b959d804e32b
NatashyaPeddle <natashya.peddle@gmail.com>
2025-11-29 13:44:25 -0500

    Login base added + input fields visible

--- project-frontend/src/Pages/Login.jsx
+++ project-frontend/src/Pages/Login.jsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
 
 /*Vendors must login to continue and then are taken to the home page for vendors: /customerreviews */
 
 
 export default function Login() {
+
+    ///state variables for email, password, error message
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
+const navigate = useNavigate();
+
+const handleLogin = async () => {
+    console.log("button clicked");
+setError("");
+try {
+    console.log("Login request sent");
+
+    const res = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+
+        
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        setError(data.error ||"Login failed");
+    }
+
+/// login succsessful
+    console.log("Login successful:", data);
+
+ 
+    ///stores token in local storage
+    localStorage.setItem("token", data.token);
+
+
+   ///moves to customer reviews page
+    navigate('/customerreviews');
+
+    ///catches errors
+} catch (error) {
+    setError("Server error: Login Failure");
+    console.error("Error during login:", error);
+}
+} 
+
+
     return (
 
         <div>
             <h2>Please login using your Vendor Email and Password:</h2>
 
-                <nav>
-                    <ul>
-                                
-                     <Link className="btn" to="/customerreviews">Login</Link> 
-                     *BUTTON IS TEMPORARY - NEEDS AUTHENTICATION ADDED*
-                                
-                    </ul>
-                        
-                </nav>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br/>
+
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /> <br/>
+
+            <button className="btn" onClick={handleLogin}>Login</button>
 
             
           
